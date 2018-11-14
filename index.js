88caaef10bc9291e03fe360c2b419d1bd0fa5cd4
Syed Faizan <syedfaizan2008@gmail.com>
2018-11-14 19:00:32 +0530

    - Added Readme - Clean Up

--- index.js
+++ index.js
@@ -1,74 +1,62 @@
 const inquirer = require('inquirer');
 
-// let lengthOfSubArray = null;
-// let sequenceOfTransactions = null;
-
-let userResponses = null;
-
 var questions = [{
   type: 'input',
   name: 'length',
   message: "How many distinct values to read?",
-},{
+}, {
   type: 'input',
   name: 'sequence',
   message: "Enter the Sequence",
 }]
 
-var sequence = []
-
 inquirer
   .prompt(questions)
-  .then( res => {
+  .then(res => {
+    let run1 = [];
+    let run2 = [];
+    let sum = 0;
+
+    if (!res.length) {
+      return console.error("Please enter number of distict values!");
+    }
+    if (!res.sequence) {
+      return console.error("Please enter the sequence of transactions!");
+    }
+
     let maxLength = res.length;
     let sequenceOftransactions = res.sequence.split(' ');
     sequenceOftransactions = sequenceOftransactions.slice(0, maxLength);
-    let sequence = sequenceOftransactions.map( item => {
+    let sequence = sequenceOftransactions.map(item => {
       return parseInt(item);
     })
 
-    let run1 = [];
 
-    sequence.forEach( (num, index) => {
-      if( run1.length == 0 ){
+    sequence.forEach((num, index) => {
+      if (run1.length == 0) {
         run1.push(num);
-      } else{
-        run1.push( run1[index -1] + num );
+      } else {
+        run1.push(run1[index - 1] + num);
       }
-      
     });
 
-    let run2 = [];
-
-    sequence.reverse().forEach( (num, index) => {
-      if( run2.length == 0 ){
+    sequence.reverse().forEach((num, index) => {
+      if (run2.length == 0) {
         run2.push(num);
-      } else{
-        run2.push( run2[index -1] + num );
+      } else {
+        run2.push(run2[index - 1] + num);
       }
-      
     });
+
     run2 = run2.reverse();
 
-    // console.log(run1);
-    // console.log(run2);
-
-    let indexesToSum = [];
-
-    for( var i = 0 ; i < maxLength-1 ; i++ ){
-      if( run1[i] == run2[i]){
-        indexesToSum.push(i);
+    // Finds the indexes where the values of both arrays are same and sums them
+    for (var i = 0; i < maxLength - 1; i++) {
+      if (run1[i] === run2[i]) {
+        sum += i;
       }
     }
 
-    console.log("Sum of Indexes: ",indexesToSum.reduce( (num, sum) => num+ sum));
-    
-
-
-
-
-
-
+    console.log("Sum of Indexes:", sum);
 
   })
-
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Problem Solving: Bank Account Trasnactions
+
+### Prerequisites:
+- NodeJS +6 installed
+
+````
+$ cd wake-assignment-1\
+$ npm install
+$ npm start
+````
+### Example
+
+````
+$ ? How many distinct values to read? 11
+$ ? Enter the Sequence 3 -2 2 0 3 4 -6 3 5 -4 8
+$ Sum of Indexes:  5
+````
+
+###### Syed Faizan
